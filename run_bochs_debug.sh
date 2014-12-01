8f936e07ecb5af4053ad7d457a32e69695cb357e
Alex Auvolat <alex.auvolat@ens.fr>
2014-12-01 17:27:57 +0100

    Change qemu/bochs launch scripts.

--- run_bochs_debug.sh
+++ run_bochs_debug.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 make -C kernel
 ./make_cdrom.sh
-bochs -f bochs.cfg -q
+bochs -f bochs_debug.cfg -q
--- run_qemu.sh
+++ run_qemu.sh
@@ -3,5 +3,4 @@
 cd `dirname $0`
 
 make -C kernel
-qemu-system-i386 -kernel kernel/kernel.bin -serial stdio -s -S &
-(sleep 0.1; gdb kernel.bin -x gdb_cmd)
+qemu-system-i386 -kernel kernel/kernel.bin -serial stdio
